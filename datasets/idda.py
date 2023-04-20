df4f22817ab38de559b90ff215a868c320bd07d8
Lorenzo Sibille <ollobille00@hotmail.it>
2023-04-20 12:10:08 +0200

    Some functions for IDDA dataset implemented Some tests on IDDA on a new file for step 1

--- datasets/idda.py
+++ datasets/idda.py
@@ -3,11 +3,26 @@ from typing import Any
 import numpy as np
 from PIL import Image
 from torch import from_numpy
+from torchvision import transforms
 from torchvision.datasets import VisionDataset
 import datasets.ss_transforms as tr
+import matplotlib.pyplot as plt
+
+def showIDDA(sample):
+    #fig,(ax1,ax2)=plt.subplots(2)
+    #ax1=plt.imshow(sample[0].permute(1, 2, 0)) #tensors in pytorch are channel first, need reshape
+    #ax2=plt.imshow(sample[1])
+    fig=plt.figure()
+    fig.add_subplot(2,1,1)
+    plt.imshow(sample[0].permute(1, 2, 0))
+    plt.axis('off')
+    fig.add_subplot(2,1,2)
+    plt.imshow(sample[1])
+    plt.axis('off')
+
 
 class_eval = [255, 2, 4, 255, 11, 5, 0, 0, 1, 8, 13, 3, 7, 6, 255, 255, 15, 14, 12, 9, 10]
-
+convert_tensor=transforms.ToTensor()
 
 class IDDADataset(VisionDataset):
 
@@ -31,7 +46,14 @@ class IDDADataset(VisionDataset):
 
     def __getitem__(self, index: int) -> Any:
         # TODO: missing code here!
-        raise NotImplementedError
+        image=Image.open(self.root+'/images/'+self.list_samples[index]+'.jpg')
+        image=convert_tensor(image)
+        #to have pixels first instead of channel first:
+        # image=from_numpy(np.asarray(image).copy())
+        target=Image.open(self.root+'/labels/'+self.list_samples[index]+'.png')
+        #TODO: capire perché,se e come usare il target_transform
+        #target=self.target_transform(np.asarray(target))
+        return image, target
 
     def __len__(self) -> int:
         return len(self.list_samples)
