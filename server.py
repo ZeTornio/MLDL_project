44a2b5d91c39359f0cbf5724d2fdfda7ae12390f
Lorenzo Sibille <ollobille00@hotmail.it>
2023-05-10 19:21:26 +0200

    tests

--- server.py
+++ server.py
@@ -91,9 +91,10 @@ class Server:
         self.metrics['eval_train'].reset()
 
         for client in self.train_clients:
+            print(f"Evaluating client {client.name}")
             client.model.load_state_dict(self.model_params_dict)
             loss,samples=client.test(self.metrics['eval_train'])
-            print(f"Client {client.name}: loss={loss}  samples={samples}")
+            print(f"\tloss={loss}  samples={samples}")
         
         self.metrics['eval_train'].get_results()
         print(f"Complexive results:{self.metrics['eval_train']}")
@@ -106,9 +107,10 @@ class Server:
         self.metrics['test_different_domain'].reset()
 
         for client in self.test_clients:
+            print(f"Evaluating client {client.name}")
             client.model.load_state_dict(self.model_params_dict)
             loss,samples=client.test(self.metrics[client.name])
-            print(f"Client {client.name}: loss={loss}  samples={samples}")
+            print(f"\tloss={loss}  samples={samples}")
 
         self.metrics['test_same_domain'].get_results()
         self.metrics['test_different_domain'].get_results()
