ba74b5897107b604b3c58363c2e449b4c4dc8c1f
Lorenzo Sibille <ollobille00@hotmail.it>
2023-05-30 13:43:05 +0200

    random seed

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
-
+import numpy as np
+import random
+import torch
 
 class HardNegativeMining(nn.Module):
 
@@ -20,3 +22,12 @@ class MeanReduction:
     def __call__(self, x, target):
         x = x[target != 255]
         return x.mean()
+
+def set_seed(random_seed):
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.cuda.manual_seed_all(random_seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
