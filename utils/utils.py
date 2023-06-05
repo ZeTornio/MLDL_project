f31c2b8adc1b66a457636d51ea17fc7b3b599307
Lorenzo Sibille <ollobille00@hotmail.it>
2023-06-05 19:49:03 +0200

    Back to merge

--- utils/utils.py
+++ utils/utils.py
@@ -22,12 +22,3 @@ class MeanReduction:
     def __call__(self, x, target):
         x = x[target != 255]
         return x.mean()
-
-def set_seed(random_seed):
-    random.seed(random_seed)
-    np.random.seed(random_seed)
-    torch.manual_seed(random_seed)
-    torch.cuda.manual_seed(random_seed)
-    torch.cuda.manual_seed_all(random_seed)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
