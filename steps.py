6f209d8083d46f32add1754fe979698b93b74ccc
Lorenzo Sibille <ollobille00@hotmail.it>
2023-06-22 16:30:47 +0200

    Debug

--- steps.py
+++ steps.py
@@ -181,6 +181,8 @@ def createServerStep4(args,train_transform,test_transform, rootIdda='data/idda',
     return Server(args=args,train_clients=train_clients,test_clients=test_clients, model=model,metrics=metrics)
 
 def createServerStep5clustering(args,train_transform,test_transform, rootIdda='data/idda', model=None):
+    if not args.clustering:
+        raise ValueError("Please set clustering=True in your args.")
     if model==None:
         model = deeplabv3_mobilenetv2(16)
     model.to(torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
